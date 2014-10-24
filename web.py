4dade2c40ebbbbdb38aa9f85dcb08623e1e55aaf
Jeremy Carbaugh <jcarbaugh@gmail.com>
2014-10-24 12:30:30 -0400

    add basscss, make dropbox service work, get upload stuff started

--- web.py
+++ web.py
@@ -1,18 +1,130 @@
+import datetime
 import os
 
+try:
+    from urlparse import urlparse       # python 2.x
+except:
+    from urllib.parse import urlparse   # python 3.x
+
 from dropbox.client import DropboxClient, DropboxOAuth2Flow
-from flask import Flask, abort, flash, redirect, render_template, session, url_for
+from flask import Flask, abort, flash, jsonify, redirect, render_template, session, url_for
+from pymongo import MongoClient
+
+
+#
+# MongoDB configuration
+#
+
+mongo_url = os.environ.get('MONGOHQ_URL', 'mongodb://localhost:27017/jifbox')
+mongo_conn = MongoClient(mongo_url)
+
+mongo_params = urlparse(mongo_url)
+
+if mongo_params.username and mongo_params.password:
+    mongo_conn.authenticate(mongo_params.username, mongo_params.password)
+
+mongo = mongo_conn[mongo_params.path.strip('/')]
+
+
+#
+# services
+#
+
+class Service(object):
+
+    def __init__(self):
+        self.register()
+
+    def register(self):
+        self._config = mongo.services.find_one({'service_id': self.service_id})
+        if not self._config:
+            doc = {
+                'service_id': self.service_id,
+            }
+            doc_id = mongo.services.save(doc)
+            self._config = mongo.services.find_one(doc_id)
+
+    def __delitem__(self, key):
+        if key in self._config:
+            del self._config[key]
+            mongo.services.save(self._config)
+
+    def __getitem__(self, key):
+        return self._config.get(key)
+
+    def __setitem__(self, key, value):
+        self._config[key] = value
+        mongo.services.save(self._config)
+
+    def process(self, payload):
+        raise NotImplementedError('process must be implemented by services')
+
+
+class DropboxService(Service):
+
+    service_id = 'dropbox'
+
+    def __init__(self):
+        super(DropboxService, self).__init__()
+        self.client_key = os.environ.get('DROPBOX_KEY')
+        self.client_secret = os.environ.get('DROPBOX_SECRET')
+
+        access_token = os.environ.get('DROPBOX_TOKEN')
+        if access_token:
+            self['access_token'] = access_token
+
+    @property
+    def is_available(self):
+        return bool(self.client_key and self.client_secret)
+
+    @property
+    def is_enabled(self):
+        return bool(self['access_token'])
+
+    def process(self, payload):
+        client = DropboxClient(self['access_token'])
+
+        overwrite = bool(payload.get('overwrite'))
+        client.put_file(payload['filename'], payload['data'], overwrite=overwrite)
+
+        return True
+
+
+class TumblrService(Service):
+
+    service_id = 'tumblr'
+
+    def __init__(self):
+        super(TumblrService, self).__init__()
+        self.client_key = os.environ.get('TUMBLR_KEY')
+        self.client_secret = os.environ.get('TUMBLR_SECRET')
+
+    @property
+    def is_available(self):
+        return bool(self.client_key and self.client_secret)
+
+    @property
+    def is_enabled(self):
+        return False
+
+    def process(self, payload):
+        pass
+
+
+services = {
+    'dropbox': DropboxService(),
+    'tumblr': TumblrService(),
+}
+
 
 #
 # Dropbox configuration
 #
 
-DROPBOX_KEY = os.environ.get('DROPBOX_KEY')
-DROPBOX_SECRET = os.environ.get('DROPBOX_SECRET')
-
 def dropbox_auth_flow():
+    dropbox = services['dropbox']
     redirect_uri = url_for('dropbox_callback', _external=True)
-    flow = DropboxOAuth2Flow(DROPBOX_KEY, DROPBOX_SECRET,
+    flow = DropboxOAuth2Flow(dropbox.client_key, dropbox.client_secret,
         redirect_uri, session, 'dropbox-auth-csrf-token')
     return flow
 
@@ -29,13 +141,32 @@ def index():
     return render_template('jifbox.html')
 
 
+@app.route('/giffed', methods=['POST'])
+def giffed():
+
+    response = {'active_services': []}
+
+    timestamp = datetime.datetime.utcnow()
+
+    payload = {
+        'filename': '%s.txt' % timestamp.isoformat(),
+        'data': timestamp.isoformat(),
+        'overwrite': False,
+        'timestamp': timestamp,
+    }
+
+    for sid, service in services.items():
+        if service.is_available and service.is_enabled:
+            if service.process(payload):
+                response['active_services'].append(sid)
+
+    return jsonify(response)
+
+
 @app.route('/settings')
 def settings():
-
-    context = {}
-
-    context['DROPBOX_AVAILABLE'] = DROPBOX_KEY and DROPBOX_SECRET
-    context['DROPBOX_AUTHED'] = False  # see if access_token is available
+    context = {'services': services}
+    return render_template('settings.html', **context)
 
 
 @app.route('/settings/dropbox/auth')
@@ -62,14 +193,14 @@ def dropbox_callback():
         app.logger.exception("Auth error" + e)
         abort(403)
 
-    # save access_token
+    services['dropbox']['access_token'] = access_token
 
     return redirect(url_for('home'))
 
 
 @app.route('/settings/dropbox/logout')
 def dropbox_logout():
-    # delete Dropbox access_token
+    del services['dropbox']['access_token']
     return redirect(url_for('settings'))
 
 
